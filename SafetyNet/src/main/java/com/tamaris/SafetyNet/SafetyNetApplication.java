1cef62a0d209a23ccad6961f35f91c0bd82142bb
tamtech10 <tamtech.dev@gmail.com>
2026-03-24 15:25:16 +0100

    app creation

---
+++ SafetyNet/src/main/java/com/tamaris/SafetyNet/SafetyNetApplication.java
@@ -0,0 +1,13 @@
+package com.tamaris.SafetyNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyNetApplication.class, args);
+	}
+
+}
---
+++ SafetyNet/src/test/java/com/tamaris/SafetyNet/SafetyNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamaris.SafetyNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
