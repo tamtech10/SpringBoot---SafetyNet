ba6f8be0f2fd5bd623a8b34f552a5ee80d357106
tamtech10 <tamtech.dev@gmail.com>
2026-03-31 16:09:18 +0200

    This URL will return a list of children (any individual aged 18 or younger) living at requested address

--- SafetyNet/src/main/java/com/tamaris/SafetyNet/controller/PersonController.java
+++ SafetyNet/src/main/java/com/tamaris/SafetyNet/controller/PersonController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping
@@ -14,15 +15,15 @@ public class PersonController {
     @Autowired
     PersonService personService;
 
-//    @GetMapping("/get")
-//    public List<Person> getPersons() {
-//        return personService.getPersons();
-//
-//    }
-
 
     @GetMapping("/communityEmail")
     public List<String> getEmailFromPeopleInSpecificCity(@RequestParam(name="city") String city) {
         return personService.getEmailFromPeopleInSpecificCity(city);
+    }
+
+    //Cette url doit retourner une liste d'enfants (tout individu âgé de 18 ans ou moins) habitant à cette adresse
+    @GetMapping("/childAlert")
+    public Map<String, Object> getInfoChild(@RequestParam(name = "address") String address) {
+        return personService.getInfoChild(address);
     }
 }
--- SafetyNet/src/main/java/com/tamaris/SafetyNet/service/PersonService.java
+++ SafetyNet/src/main/java/com/tamaris/SafetyNet/service/PersonService.java
@@ -1,23 +1,29 @@
 package com.tamaris.SafetyNet.service;
 
+import com.tamaris.SafetyNet.model.Medicalrecord;
 import com.tamaris.SafetyNet.model.Person;
+import com.tamaris.SafetyNet.repository.MedicalrecordRepository;
 import com.tamaris.SafetyNet.repository.PersonRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 @Service
 public class PersonService {
 
     private final PersonRepository personRepository;
+    private final MedicalrecordRepository medicalrecordRepository;
 
-    public PersonService(PersonRepository personRepository) {
+    public PersonService(PersonRepository personRepository, MedicalrecordRepository medicalrecordRepository) {
         this.personRepository = personRepository;
+        this.medicalrecordRepository = medicalrecordRepository;
     }
 
 
-    //Cette url doit retourner les adresses mail de tous les habitants de la ville
+    //*******Cette url doit retourner les adresses mail de tous les habitants de la ville
     public ArrayList<String> getEmailFromPeopleInSpecificCity(String city) {
 
         ArrayList<String> emails = new ArrayList<>();
@@ -31,7 +37,62 @@ public class PersonService {
         return emails;
     }
 
-//    public ArrayList<String> getEmailFromPeopleInSpecificCity(String city) {
-//        return personRepository.getEmailFromPeopleInSpecificCity(city);
-//    }
+
+    //******Cette url doit retourner une liste d'enfants (tout individu âgé de 18 ans ou moins) habitant à cette adresse
+    public Map<String, Object> getInfoChild(String address) {
+
+        List<Person> persons = personRepository.findAllPersons();
+        List<Medicalrecord> records = medicalrecordRepository.findAllMedicalrecords();
+
+        List<Map<String, Object>> children = new ArrayList<>();
+        List<Map<String, String>> members = new ArrayList<>();
+
+        for (Person p : persons) {
+            if (p.getAddress().equals(address)) {
+
+                Medicalrecord medicalrecord = null;
+
+                for (Medicalrecord mr : records) {
+                    if (mr.getFirstName().equals(p.getFirstName()) && mr.getLastName().equals(p.getLastName())) {
+                        medicalrecord = mr;
+                        break;
+                    } //je m'assure que je suis sur la mm personne
+                }
+                //Si je trouvais un dossier médical pour cette personne, alors je calcul son âge.
+                if (medicalrecord != null) {
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy"); //our string into an object
+                    LocalDate birth = LocalDate.parse(medicalrecord.getBirthdate(), formatter);
+                    int age = calculateAge(birth);
+
+                    if(age <= 18) {
+                        //ako imaju manje ili jednako kreiramo prvi map gde cemo ih skladistiti
+                        Map<String, Object> child = new HashMap<>();
+                        child.put("firstName", p.getFirstName());
+                        child.put("lastName", p.getLastName());
+                        child.put("age", age);
+                        children.add(child);
+                    } else { //ako je odrastao stavljam ga u map pa taj map u listu
+                        Map<String, String> member = new HashMap<>();
+                        member.put("firstName", p.getFirstName());
+                        member.put("lastName", p.getLastName());
+                        members.add(member);
+                    }
+                }
+            }
+        }
+
+        //skladistimo sve rezultate u jedan map
+        Map<String, Object> results = new HashMap<>();
+        results.put("Children", children);
+        results.put("Members", members);
+
+        return results;
+    }
+
+    //racunamo godine
+    private int calculateAge(LocalDate birthDate) {
+        return Period.between(birthDate, LocalDate.now()).getYears();
+    }
+
+
 }
